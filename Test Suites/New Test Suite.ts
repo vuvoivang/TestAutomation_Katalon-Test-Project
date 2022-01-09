65ee1c30d53570de86ea991790cab0e8aa89a424
DuyOnix <dtduy2210@gmail.com>
2022-01-09 11:16:03 +0700

    handle teacher TC

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,140 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b9bf84c9-9eb2-4aca-9509-0b657d142400</testSuiteGuid>
+   <testCaseLink>
+      <guid>5588d26a-4ea6-4f29-9047-492cf9689355</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Announcement/Teacher_Add Annoucement Advanced With Tags</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6f89a300-0c2c-4bca-aa75-eb50b27274fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Announcement/Teacher_Add Annoucement Basic Valid All Required Field</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d7c995aa-4da4-4cbb-8409-a16cdd76067d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Announcement/Teacher_Add Announcement Advanced With Display Period</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c664300-8e6c-4044-bc44-6ab642ddb783</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Announcement/Teacher_Add Announcement Submit Without Input</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0c1c96a5-00b4-4d09-af98-828b1d2edd58</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Assignment/Teacher_Add Assignment Submit Without Input</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>306d5002-b28e-4da2-a57f-bd0fc111d76f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Assignment/Teacher_Add Assignment Valid All Required Field</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4d34aa3c-d88d-43a4-98f6-d18b88115d65</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Assignment/Teacher_Add Assignment With Due Date Before Allow Submission From</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f78be6b7-16fe-44fe-905e-b439d0eefed6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Assignment/Teacher_Add Assignment With Submission Type</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>99643b55-4d50-47b4-ae3f-923b240b311b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Forum/Teacher_Add Forum Submit Without Input in Forum</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d257cddb-fa3a-4ecb-beaf-af670ebc589e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Forum/Teacher_Add Forum Submit Without Input in Topic</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>408d8107-cf09-4a51-b028-35f9e0ed394d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Forum/Teacher_Add Forum With Topic Valid All Required</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>160366dc-9176-4b3f-95f7-52c58d7736b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Quiz/Teacher_Add A Short Answer Question With No Full Grade Answer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3ee3ea8d-cc7e-454e-8c5a-6c61ce98dc9e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Quiz/Teacher_Add Quiz Basic With Missing Name</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dafb3b7b-0a3f-4d65-b82f-e61123777fea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Quiz/Teacher_Quiz Add A Multiple Choices Question Successfully</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5b8ab980-73c6-4584-b1ab-4bb56364c088</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Quiz/Teacher_Quiz Add A Multiple Choices Question With Only One Answer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c17b2d0d-8a34-4e28-b82f-8306aa1f57db</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Quiz/Teacher_Quiz Add Question With Missing Question Text</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4147d920-6ce7-4c8f-8201-686d8584017b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Quiz Result/Teacher_Review An Attempt</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c6579af9-9806-4e9b-87a4-e093b3ca2363</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Quiz Result/Teacher_See The Statistics Of A Question</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fa299ba7-5a2c-4441-9651-74b1715ed012</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Quiz Result/Teacher_See Unattempted Students In A Quiz</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>00c6eee1-1813-40ba-aa68-0406f722f0bf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Survey/Teacher_Add Survey ATTLS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8ecc1b7d-d66b-4de7-b4f8-ed4fd2e0c349</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teacher/Teacher_Add Survey/Teacher_Add Survey Critical Incidents</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
