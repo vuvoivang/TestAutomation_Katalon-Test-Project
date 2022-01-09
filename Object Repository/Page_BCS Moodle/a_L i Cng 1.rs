5a4edb972a63a3f6e7800b20ad5bf269bcd2e53a
ydam <ydamk15nbk@gmail.com>
2022-01-09 11:15:10 +0700

    healing

--- Object Repository/Page_BCS Moodle/a_L i Cng 1.rs
+++ Object Repository/Page_BCS Moodle/a_L i Cng 1.rs
@@ -14,7 +14,7 @@
          <value>a.aalink</value>
       </entry>
    </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
+   <selectorMethod>CSS</selectorMethod>
    <useRalativeImagePath>true</useRalativeImagePath>
    <webElementProperties>
       <isSelected>false</isSelected>
